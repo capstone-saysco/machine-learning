04746779cb230a81e6489bc132f171b59047cafb
RowPramudito <danielrowang@gmail.com>
2023-12-05 18:06:30 +0700

    Initial commit

---
+++ softattention.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# @Author: colinzhang
+# @Date:   9/12/19 2:57 PM
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import numpy as np
+
+from tensorflow import keras
+from keras.layers import Layer
+from keras import backend as K
+
+
+class Attention(Layer):
+    def __init__(self, op='attsum', activation='tanh', init_stdev=0.01, **kwargs):
+        self.supports_masking = True
+        assert op in {'attsum', 'attmean'}
+        assert activation in {None, 'tanh'}
+        self.op = op
+        self.activation = activation
+        self.init_stdev = init_stdev
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        init_val_v = (np.random.randn(input_shape[2]) * self.init_stdev).astype(K.floatx())
+        self.att_v = tf.Variable(init_val_v, name='att_v', trainable=True)
+        init_val_W = (np.random.randn(input_shape[2], input_shape[2]) * self.init_stdev).astype(K.floatx())
+        self.att_W = tf.Variable(init_val_W, name='att_W', trainable=True)
+
+    def call(self, inputs, mask=None):
+        y = K.dot(inputs, self.att_W)
+        if not self.activation:
+            weights = tf.tensordot(self.att_v, y, axes=[0, 2])
+        elif self.activation == 'tanh':
+            weights = tf.tensordot(self.att_v, K.tanh(y), axes=[[0], [2]])
+
+        weights = K.softmax(weights)
+
+        out = inputs * K.permute_dimensions(K.repeat(weights, inputs.shape[2]), [0, 2, 1])
+        if self.op == 'attsum':
+            # out = out.sum(axis=1)
+            out = K.sum(out, axis=1)
+        elif self.op == 'attmean':
+            out = out.sum(axis=1) / mask.sum(axis=1, keepdims=True)
+        return K.cast(out, K.floatx())
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[2])
+
+    def compute_mask(self, x, mask):
+        return None
+
+    def get_config(self):
+        config = {'op': self.op, 'activation': self.activation, 'init_stdev': self.init_stdev}
+        base_config = super(Attention, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
---
+++ zeromasking.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @Author: colinzhang
+# @Date:   9/12/19 2:43 PM
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from keras.layers import Layer
+import keras.backend as K
+
+
+class ZeroMaskedEntries(Layer):
+    """
+    This layer is called after an Embedding layer.
+    It zeros out all of the masked-out embeddings.
+    It also swallows the mask without passing it on.
+    You can change this to default pass-on behavior as follows:
+
+    def compute_mask(self, x, mask=None):
+        if not self.mask_zero:
+            return None
+        else:
+            return K.not_equal(x, 0)
+    """
+
+    def __init__(self, mask_zero=False, **kwargs):
+        super(ZeroMaskedEntries, self).__init__(**kwargs)
+        self.mask_zero = mask_zero
+
+    def build(self, input_shape):
+        self.output_dim = input_shape[1]
+        self.repeat_dim = input_shape[2]
+
+    def call(self, inputs, mask=None):
+        mask = K.cast(mask, 'float32')
+        mask = K.repeat(mask, self.repeat_dim)
+        mask = K.permute_dimensions(mask, (0, 2, 1))
+        return inputs * mask
+
+    def compute_mask(self, inputs, mask=None):
+        if not self.mask_zero:
+            return None
+        else:
+            return tf.not_equal(inputs, 0)
